4b4435ee79b8580654a9951e462f011a385b41d8
Genexyz <gianlucapagano14@gmail.com>
2023-03-04 00:48:24 -0300

    Layout, Sign in and Sign up

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,235 @@
-import { Outlet } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import CategoryIcon from "@mui/icons-material/Category";
+import { Outlet, Link as RouterLink, useNavigate } from "react-router-dom";
+import Link from "@mui/material/Link";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = (): JSX.Element => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const navigate = useNavigate();
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (): void => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = (): void => {
+    setAnchorElUser(null);
+  };
+
+  const logoutUser = (): void => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+    setAnchorElUser(null);
+    toast.success("Signed out successfully");
+    navigate("/signin");
+  };
+
+  const userImageUrl = "https://cdn-icons-png.flaticon.com/512/1144/1144709.png";
+
+  let user = false;
+  const result = localStorage.getItem("user");
+  if (result !== null && typeof result === "string") {
+    user = true;
+  }
+
   return (
     <>
-      <div></div>
+      <AppBar position="static" color="secondary">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <CategoryIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Link to="/" component={RouterLink} color="inherit" underline="none">
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                PRODUCTS-APP
+              </Typography>
+            </Link>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <Link
+                  to="/products"
+                  component={RouterLink}
+                  color="inherit"
+                  underline="none"
+                >
+                  <MenuItem key="products" onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Products</Typography>
+                  </MenuItem>
+                </Link>
+                <Link
+                  to="/reviews"
+                  component={RouterLink}
+                  color="inherit"
+                  underline="none"
+                >
+                  <MenuItem key="reviews" onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Reviews</Typography>
+                  </MenuItem>
+                </Link>
+              </Menu>
+            </Box>
+            <CategoryIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Link to="/" component={RouterLink} color="inherit" underline="none">
+              <Typography
+                variant="h5"
+                noWrap
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+                textAlign="center"
+              >
+                PRODUCTS-APP
+              </Typography>
+            </Link>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Link
+                to="/products"
+                component={RouterLink}
+                color="inherit"
+                underline="none"
+              >
+                <Button
+                  key="products"
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  Products
+                </Button>
+              </Link>
+              <Link to="/reviews" component={RouterLink} color="inherit" underline="none">
+                <Button
+                  key="reviews"
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  Reviews
+                </Button>
+              </Link>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="user icon" src={userImageUrl} />
+              </IconButton>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {/* TODO: Repeated validations to quickly bypass a MUI error with React Fragments */}
+                {!user && (
+                  <Link
+                    to="/signin"
+                    component={RouterLink}
+                    color="inherit"
+                    underline="none"
+                  >
+                    <MenuItem key="signin" onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">Sign in</Typography>
+                    </MenuItem>
+                  </Link>
+                )}
+                {!user && (
+                  <Link
+                    to="/signup"
+                    component={RouterLink}
+                    color="inherit"
+                    underline="none"
+                  >
+                    <MenuItem key="signup" onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">Sign up</Typography>
+                    </MenuItem>
+                  </Link>
+                )}
+                {user && (
+                  <MenuItem key="signout" onClick={logoutUser}>
+                    <Typography textAlign="center">Sign out</Typography>
+                  </MenuItem>
+                )}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <Outlet />
+      <ToastContainer autoClose={2500} />
     </>
   );
 };
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,8 +1,199 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const MIN_PASSWORD_LENGTH = 8;
+const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL as string;
+
+interface User {
+  email?: string;
+  password?: string;
+  name?: string;
+}
+
 const Signup = (): JSX.Element => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [errors, setErrors] = useState<User>({});
+  const navigate = useNavigate();
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    if (errors[name as keyof User] != null)
+      setErrors({
+        ...errors,
+        [name]: null,
+      });
+  };
+
+  const findFormErrors = (): User => {
+    const { email, password, name } = user;
+
+    const newErrors: User = {};
+    if (email === "") newErrors.email = "Email is required";
+    if (email.length === 0) newErrors.email = "Email is required";
+    else if (!EMAIL_REGEX.test(email)) newErrors.email = "Invalid email address";
+    if (password === "") newErrors.password = "Password is required";
+    else if (!PASSWORD_REGEX.test(password))
+      newErrors.password = `Password must be at least ${MIN_PASSWORD_LENGTH} characters long and contain at least one letter, one number, and one special character`;
+    if (name === "") newErrors.name = "Name is required";
+    if (name.length > 100) newErrors.name = "Name cannot be more than 100 characters";
+
+    return newErrors;
+  };
+
+  const submitData = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    const newErrors = findFormErrors();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      setErrors({});
+      try {
+        const body = {
+          ...user,
+        };
+        const response = await fetch(`${SERVER_URL}/auth/signup`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+        const data = await response.json();
+        if (response.status === 201) {
+          console.log("User Signed Up!");
+          console.log(data);
+          toast.success("User Signed Up!");
+          storeData(data.token, data.refreshToken, JSON.stringify(data.user));
+          navigate("/");
+        }
+        if (response.status === 400) {
+          if (data.message === "Email already registered") {
+            setErrors({ email: data.message });
+          } else {
+            setErrors(data);
+          }
+          toast.error("User not signed up, check errors!");
+          console.log(data);
+        }
+        if (response.status === 401) {
+          console.log(data);
+          toast.error("Unauthorized!");
+        }
+        if (response.status === 500) {
+          console.log(data);
+          toast.error("Server Error!");
+        }
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    }
+  };
+
+  const storeData = (accessToken: string, refreshToken: string, user: string): void => {
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+    localStorage.setItem("user", user);
+  };
+
   return (
-    <div>
-      <h1>Sign up</h1>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "black" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <Box component="form" noValidate onSubmit={submitData} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="name"
+                name="name"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                autoFocus
+                value={user.name}
+                onChange={onInputChange}
+                error={Boolean(errors.name)}
+                helperText={errors.name}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={user.email}
+                onChange={onInputChange}
+                error={Boolean(errors.email)}
+                helperText={errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                value={user.password}
+                onChange={onInputChange}
+                error={Boolean(errors.password)}
+                helperText={errors.password}
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link component={RouterLink} to="/signin" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
